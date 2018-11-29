696fa61ff91029385ce6aedbe9b1130e8ea01a31
Otoniel Maya <otto94@gmail.com>
2018-11-29 14:22:04 -0600

    !

--- databases.sh
+++ databases.sh
@@ -29,15 +29,15 @@ dbNCBI="$HOME/bin/16S/NCBI.gz"
 dbRDP="$HOME/bin/16S/RDP.gz"
 dbSILVA="$HOME/bin/16S/SILVA.gz"
 
-if [ -f "$dbNCBI" ]; then
+if [ ! -f "$dbNCBI" ]; then
   esearch -db nucleotide -query '33175[BioProject] OR 33317[BioProject]' \
     | efetch -db nuccore -format fasta | gzip -9 > $HOME/bin/16S/NCBI.gz
-elif [ -f "$dbRDP" ]; then
+elif [ ! -f "$dbRDP" ]; then
   wget --no-check-certificate https://rdp.cme.msu.edu/download/current_Bacteria_unaligned.fa.gz
     gunzip -c current_Bacteria_unaligned.fa.gz \
     | bioawk -cfastx '/\(T\)/{print ">" $name " " $comment "\n" toupper($seq)}' | gzip -9 \
     > $HOME/bin/16S/RDP.gz
-elif [ -f "$dbSILVA" ]; then
+elif [ ! -f "$dbSILVA" ]; then
   wget https://www.arb-silva.de/fileadmin/silva_databases/release_132/Exports/SILVA_132_SSURef_Nr99_tax_silva.fasta.gz
   gunzip -v SILVA_132_SSURef_Nr99_tax_silva.fasta.gz \
     | bioawk -cfastx \
