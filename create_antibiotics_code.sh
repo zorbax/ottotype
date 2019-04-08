3c4e05fa57ad3274ac7434c6d334886122663cc0
Otoniel Maya <otto94@gmail.com>
2019-04-07 23:45:30 -0500

    argannot update

--- create_antibiotics_code.sh
+++ create_antibiotics_code.sh
@@ -2,40 +2,38 @@
 
 grep < ARGannot_r3.fasta \> | sed 's/ /;/' | \
     tr ';' '\t' | awk -v OFS='\t' '{ print $5, $4}' | \
-    tee >( sort -k1,2 | sed 's/Agly/AGly/' > code.raw.tsv) \
-    >( cut -f1 | sort | uniq > categories.txt) \
+    sed 's/Agly/AGly/g' | tee >( sort -k1,2 > code.raw.tsv)\
+    >( cut -f1 | sort | uniq > categories.txt)\
     > /dev/null
 
 samples=(
-         AGly:Aminoglucósidos
-         Agly:Aminoglucósidos
-         Bla:Betalactámicos
-         Colistin:Colistina
-         Fcd:
-         Fcyn:Fosfomicina
-         Flq:Fluoroquinolonas
-         Gly:Glicopéptidos
-         MLS:Macrólidos-Lincosamidas-Estreptograminas
-         Ntmdz:Nitroimidazol
-         Oxzln:Oxazolidinonas
-         Phe:Fenicoles
-         Rif:Rifampicina
-         Sul:Sulfonamidas
-         Tet:Tetraciclinas
-         Tmt:Trimetroprima
+         "AGly:Aminoglucósidos"
+         "Bla:Betalactámicos"
+         "Colistin:Colistina"
+         "Fcd:Ácido_Fusídico"
+         "Fcyn:Fosfomicina"
+         "Flq:Fluoroquinolonas"
+         "Gly:Glicopéptidos"
+         "MLS:Macrólidos-Lincosamidas-Estreptograminas"
+         "Ntmdz:Nitroimidazol"
+         "Oxzln:Oxazolidinonas"
+         "Phe:Fenicoles"
+         "Rif:Rifampicina"
+         "Sul:Sulfonamidas"
+         "Tet:Tetraciclinas"
+         "Tmt:Trimetroprima"
         )
 
 for i in "${samples[@]}"
 do
     key="${i%%:*}"
     val="${i##*:}"
-while read categorie
-do
-    if key == categorie
-        sed -i 's/categorie/value/' code.raw.tsv
-    else
-        echo"not foud this gen in samples categories" (print value of $samples[@]) 
-    fi
-done < categories.txt
-
-fi
+    while read id_cat
+    do
+        if [ ${key} == ${id_cat} ]; then
+            sed "s/$id_cat/$val/g" code.raw.tsv >> test.tsv
+        else
+            echo -e "$id_cat category not found in: \n${samples[@]}" 
+        fi
+    done < <(cat categories.txt | cut -f1 | uniq)
+done
