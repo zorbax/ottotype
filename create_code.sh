cdfea4c07f854e62e0721a9655a8f3f70ad5e329
Otoniel Maya <otto94@gmail.com>
2019-04-08 23:57:38 -0500

    test create code

--- create_code.sh
+++ create_code.sh
@@ -1,14 +1,14 @@
-#!/bin/bash  
+#!/bin/bash
 
 grep < ARGannot_r3.fasta \> | sed 's/ /;/' | \
-    tr ';' '\t' | awk -v OFS='\t' '{ print $5, $4}' | \
-    sed 's/Agly/AGly/g' | tee >( sort -k1,2 > code.raw.tsv)\
-    >( cut -f1 | sort | uniq > categories.txt)\
-    > /dev/null
+  tr ';' '\t' | awk -v OFS='\t' '{ print $5, $4}' | \
+  sed 's/Agly/AGly/g' | tee >( sort -k1,2 > code.raw.tsv)\
+  >( cut -f1 | sort | uniq > categories.txt)\
+  > /dev/null
 
 samples=(
-         "AGly:Aminoglucósidos" "Bla:Betalactámicos"   
-         "Colistin:Colistina"   "Fcd:Ácido_Fusídico"  
+         "AGly:Aminoglucósidos" "Bla:Betalactámicos"
+         "Colistin:Colistina"   "Fcd:Ácido_Fusídico"
          "Fcyn:Fosfomicina"     "Flq:Fluoroquinolonas"
          "Gly:Glicopéptidos"    "MLS:Macrólidos-Lincosamidas-Estreptograminas"
          "Ntmdz:Nitroimidazol"  "Oxzln:Oxazolidinonas"
@@ -17,17 +17,17 @@ samples=(
          "Tmt:Trimetroprima"
         )
 
-while read id_cat
+while read line
 do
-    for i in "${samples[@]}"
-    do
-        key="${i%%:*}"
-        val="${i##*:}"
-        
-        if [ ${id_cat} == ${key} ] ; then
-            sed -i "0,/${id_cat}/s//${val}/ii
-           
-            " code.raw.tsv 
-    i
-    done
-done < <(cat categories.txt | cut -f1 | uniq)
+  col=$(echo $line | cut -f1)
+  gene=$(echo $line | cut -f2)
+  for k in "${samples[@]}"
+  do
+    key="${k%%:*}"
+    val="${k##*:}"
+
+    if [ ${col} == ${key} ] ; then
+      echo -e "${val}\t${gene}"
+    fi
+  done
+done < <(cat code.raw.tsv)
