1ae04bf3256596878730c683c4cc4cc6342ac732
zorbax <otto94@gmail.com>
2019-05-07 16:21:05 -0500

    test05

--- ottotype.sh
+++ ottotype.sh
@@ -488,8 +488,8 @@ run_salmonella() {
     if [ -e "mlst_report.tsv" ]; then
       cat mlst_report.tsv | tail -n+2 | awk -v var="$id" -v OFS='\t' '{ print var, $0}' | \
       sed 's/[*?]//g' | sort
-      cd ..
     fi
+    cd ..
   done > ../OUTPUT/ariba_${run_name}_achtman.tsv && cd ..
 
   cp OUTPUT/ariba_${run_name}_achtman.tsv RESULTS/ariba_${run_name}_achtman.tsv
@@ -540,7 +540,7 @@ trimming() {
     trimmomatic PE -phred33 -threads $(nproc) $r1 $r2 \
               TRIMMING/${name}_R1.trim.fastq.gz TRIMMING/1U2U/${name}.1U.trim.fastq.gz \
               TRIMMING/${name}_R2.trim.fastq.gz TRIMMING/1U2U/${name}.2U.trim.fastq.gz \
-              SLIDINGWINDOW:4:20 MINLEN:70 &> ${name}.trim.log
+              SLIDINGWINDOW:4:20 MINLEN:125 &> ${name}.trim.log
 
     if [ $? -eq 0 ]; then
       rm *trim.log
