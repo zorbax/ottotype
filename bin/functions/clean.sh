45da6e831cc4e55c43d8dc335d779eedfa108512
zorbax <otto94@gmail.com>
2019-07-23 15:10:28 -0500

    test06

--- bin/functions/clean.sh
+++ bin/functions/clean.sh
@@ -3,7 +3,7 @@
 clean() {
   find -maxdepth 1 -name "*fastq.gz" -type f -or -type l | \
      rename 's/_L001//; s/_001//; s/_1.fastq/_S01_R1.fastq/ ;
-     s/_2.fastq/_S01_R2.fastq/'
+     s/_2.fastq/_S01_R2.fastq/; s/-//'
 
   echo -e "\n\n# The filenames were renamed with the ${FUNCNAME[0]} function" &>> $log_file
 }
--- bin/functions/trimming.sh
+++ bin/functions/trimming.sh
@@ -10,7 +10,7 @@ trimming() {
     trimmomatic PE -phred33 -threads $(nproc) $r1 $r2 \
               TRIMMING/${name}_R1.trim.fastq.gz TRIMMING/1U2U/${name}.1U.trim.fastq.gz \
               TRIMMING/${name}_R2.trim.fastq.gz TRIMMING/1U2U/${name}.2U.trim.fastq.gz \
-              ILLUMINACLIP:NexteraPE-PE.fa:2:30:10 > $name.trim.log
+              ILLUMINACLIP:NexteraPE-PE.fa:2:30:10 SLIDINGWINDOW:4:20 MINLEN:240 > $name.trim.log
 
     if [ $? -eq 0 ]; then
       rm $name.trim.log
