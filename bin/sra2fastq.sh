0aa7605e91ca92f1f961048b388ae8fbbb0b8f1d
Otoniel Maya <otto94@gmail.com>
2019-08-30 21:30:34 -0500

    sra update

--- bin/sra2fastq.sh
+++ bin/sra2fastq.sh
@@ -8,20 +8,27 @@ else
   list=$@
 fi
 
-echo -e "SampleName\tRun\tBioProject\tSample\tBioSample\tScientificName" > pnusas2sra.tsv
+echo -e "SampleName\tRun\tBioExperiment\tBioProject\t\
+         Sample\tBioSample\tLibraryStrategy\tLibrarySelection\t\
+         LibrarySource\tScientificName" > pnusas2sra.tsv
 
 for i in ${list}
 do
-  table=`esearch -db sra -query ${i} | efetch -format runinfo | \
-  awk -F',' -v OFS='\t' '{ print $30, $1, $22, $25, $26, $29}' | \
-  tail -n+2`
+  table=$(esearch -db sra -query ${i} | efetch -format runinfo | \
+          awk -F',' -v OFS='\t' '{ print $30, $1, $11, $22, $25,
+              $26, $13, $14, $15, $29}' | tail -n+2)
 
   echo -e "$table\n" | sort | uniq | sed '/^$/d' >> pnusas2sra.tsv
-  sra=`echo "$table" | grep -v Run | awk '{ print $2 }' | sed '/^$/d' | sort | uniq`
+  sra=$(echo "$table" | grep -v Run | awk '{ print $2 }' | \
+        sed '/^$/d' | sort | uniq)
   echo $sra >> sra.txt
   for k in ${sra}
   do
     echo -e "${i}\t${k}"
-    fastq-dump --split-files --defline-seq '@$ac.$si.$sg/$ri' --defline-qual '+' --gzip ${k}
+    fastq-dump --split-files --defline-seq '@$ac.$si.$sg/$ri' \
+               --defline-qual '+' --gzip ${k}
+    rename 's/_1.fastq/_S01_R1_L001.fastq/ ; s/_2.fastq/_S01_R2_L001.fastq/' *gz
   done
 done
+
+rm -rf $HOME/ncbi
