744c48e91db52a03e4c956a4c68f0ecfa8751013
zorbax <otto94@gmail.com>
2022-06-02 13:55:49 +0200

    test

--- ottotype.sh
+++ ottotype.sh
@@ -41,80 +41,80 @@ echo ${run_name}
 #screen_tax
 
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | \
- xargs -r docker rmi -f &> /dev/null
+    xargs -r docker rmi -f &> /dev/null
 
 if [[ -s "SCREENING/salm_id.txt" ]]; then
-  mkdir -p SALMONELLA
+    mkdir -p SALMONELLA
 
-  while read -r fastq
-  do
-    find . -name "$fastq*fastq.gz" -type f -not -path "SALMONELLA/*" \
-           -print0 | xargs -0 mv -t "SALMONELLA/" 2>/dev/null
-  done < <(cut -f1 SCREENING/salm_id.txt)
+    while read -r fastq
+    do
+        find . -name "$fastq*fastq.gz" -type f -not -path "SALMONELLA/*" \
+            -print0 | xargs -0 mv -t "SALMONELLA/" 2>/dev/null
+    done < <(cut -f1 SCREENING/salm_id.txt)
 
-  cd SALMONELLA || exit
-  file="../SCREENING/salm_id.txt"
-  echo "SALMONELLA"
-  run_salmonella &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Trimming"
-  trimming &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Assembly"
-  assembly_idba &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Stats"
-  assembly_stats_cov &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Virulence"
-  plas_vir_vfdb &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Plasmids"
-  plasmids &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  cd ..
+    cd SALMONELLA || exit
+    file="../SCREENING/salm_id.txt"
+    echo "SALMONELLA"
+    run_salmonella &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Trimming"
+    trimming &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Assembly"
+    assembly_idba &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Stats"
+    assembly_stats_cov &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Virulence"
+    plas_vir_vfdb &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Plasmids"
+    plasmids &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    cd ..
 fi
 
 echo "SALM-LIKE"
 
 if [[ -s "SCREENING/salm-like.txt" ]]; then
-  mkdir -p SALM-LIKE
+    mkdir -p SALM-LIKE
 
-  while read -r fastq
-  do
-    find . -name "$fastq*fastq.gz" -type f -not -path "SALM-LIKE/*" \
-        -print0 | xargs -0 mv -t "SALM-LIKE/" 2>/dev/null
-  done < <(cut -f1 SCREENING/salm-like.txt)
+    while read -r fastq
+    do
+        find . -name "$fastq*fastq.gz" -type f -not -path "SALM-LIKE/*" \
+            -print0 | xargs -0 mv -t "SALM-LIKE/" 2>/dev/null
+    done < <(cut -f1 SCREENING/salm-like.txt)
 
-  cd SALM-LIKE || exit
-  file="../SCREENING/salm-like.txt"
-  echo "SALM-LIKE"
-  run_salmonella &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Trimming"
-  trimming &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Assembly"
-  assembly_idba &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Stats"
-  assembly_stats_cov &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Virulence"
-  plas_vir_vfdb &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Assembly MLST"
-  assembly_mlst &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "KmerFinder"
-  kmer_finder &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  echo "Kraken2"
-  kraken_tax &>> ${log_file} || error ${LINENO} "$(basename $0)"
-  cd ..
+    cd SALM-LIKE || exit
+    file="../SCREENING/salm-like.txt"
+    echo "SALM-LIKE"
+    run_salmonella &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Trimming"
+    trimming &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Assembly"
+    assembly_idba &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Stats"
+    assembly_stats_cov &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Virulence"
+    plas_vir_vfdb &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Assembly MLST"
+    assembly_mlst &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "KmerFinder"
+    kmer_finder &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    echo "Kraken2"
+    kraken_tax &>> ${log_file} || error ${LINENO} "$(basename $0)"
+    cd ..
 fi
 
 echo "OTHERS"
 
 if [[ -s "SCREENING/nosalm_id_ncbi.txt" ]]; then
-  file="SCREENING/nosalm_id_ncbi.txt"
+    file="SCREENING/nosalm_id_ncbi.txt"
 else
-  file="SCREENING/nosalm_id.txt"
+    file="SCREENING/nosalm_id.txt"
 fi
 
 mkdir -p OTHERS
 
 while read -r fastq
 do
-  find . -name "$fastq*fastq.gz" -type f -not -path "OTHERS/*" \
-      -print0 | xargs -0 mv -t "OTHERS/" 2>/dev/null
+    find . -name "$fastq*fastq.gz" -type f -not -path "OTHERS/*" \
+        -print0 | xargs -0 mv -t "OTHERS/" 2>/dev/null
 done < <(cat $file | cut -f1)
 
 cd OTHERS || exit
@@ -134,7 +134,7 @@ mkdir -p RESULTS_${run_name}
 
 for i in ${path_results}
 do
-  cp ${i}/* RESULTS_${run_name}
+    cp ${i}/* RESULTS_${run_name}
 done
 
 tmprm
